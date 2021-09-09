09868584c3074244b48ba95ff527e3a7236249f5
Florian Franzen <Florian.Franzen@gmail.com>
2021-09-09 14:06:39 +0200

    Cleanup demos

--- settings.py
+++ settings.py
@@ -2,72 +2,37 @@ from os import environ
 
 SESSION_CONFIGS = [
     dict(
-        name='hard_auction',
-        display_name="Hard Ending Auction",
+        name='custom_auction',
+        display_name="Auction Experiment",
         app_sequence=['slot_auction'],
-        num_demo_participants=3,
+    ),
+
+    dict(
+        name='hard_auction',
+        display_name="Demo - Hard Ending Auction",
+        app_sequence=['slot_auction'],
         num_hard_participants=3,
     ),
     dict(
         name='candle_auction',
-        display_name="Candle Auction",
+        display_name="Demo - Candle Auction",
         app_sequence=['slot_auction'],
-        num_demo_participants=3,
         num_candle_participants=3,
     ),
     dict(
         name='activity_auction',
-        display_name="Activity Rule Auction",
+        display_name="Demo - Activity Rule Auction",
         app_sequence=['slot_auction'],
-        num_demo_participants=3,
         num_activity_participants=3,
     ),
 
     dict(
-        name='hard_auction_4',
-        display_name="Hard Ending Auction (4 Slots)",
+        name='complex_auction',
+        display_name="Demo - Overlapping Slot Auction",
         app_sequence=['slot_auction'],
-        num_demo_participants=3,
-        num_hard_participants=3,
-        num_global_slots=4,
-        num_local_slots=2
-    ),
-    dict(
-        name='candle_auction_4',
-        display_name="Candle Auction (4 Slots)",
-        app_sequence=['slot_auction'],
-        num_demo_participants=3,
-        num_candle_participants=3,
-        num_global_slots=4,
-        num_local_slots=2
-    ),
-    dict(
-        name='activity_auction_4',
-        display_name="Activity Rule Auction (4 Slots)",
-        app_sequence=['slot_auction'],
-        num_demo_participants=3,
-        num_activity_participants=3,
-        num_global_slots=4,
-        num_local_slots=2
-    ),
-
-    dict(
-        name='hard_auction_6',
-        display_name="Hard Ending Auction (6 Slots)",
-        app_sequence=['slot_auction'],
-        num_demo_participants=3,
-        num_hard_participants=3,
-        num_global_slots=6,
-        num_local_slots=1
-    ),
-    dict(
-        name='hard_auction_9',
-        display_name="Hard Ending Auction (9 Slots)",
-        app_sequence=['slot_auction'],
-        num_demo_participants=3,
         num_hard_participants=3,
         num_global_slots=9,
-        num_local_slots=3
+        num_local_slots=3,
     ),
 ]
 
@@ -79,8 +44,8 @@ SESSION_CONFIGS = [
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point=1.00, participation_fee=0.00,
     doc="Please ensure adjustment of participants per treatment in 'Configure session' section.",
-    num_global_slots=2, num_local_slots=1,
-    num_hard_participants=0, num_candle_participants=0, num_activity_participants=0
+    num_hard_participants=0, num_candle_participants=0, num_activity_participants=0,
+    num_demo_participants=3, num_global_slots=2, num_local_slots=1, 
 )
 
 PARTICIPANT_FIELDS = ['role', 'treatment', 'finished']

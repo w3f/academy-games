18911c358635cb98cb825f93e3dad8b899a50aec
Florian Franzen <Florian.Franzen@gmail.com>
2022-07-08 12:29:39 +0200

    settings: split configs by project and dry up academy configs

---
+++ academy.py
@@ -0,0 +1,101 @@
+"""otree session and room configs for blockchain academy."""
+
+import importlib
+
+
+# List of all game identifiers
+ACADEMY_GAME_CONFIGS = {
+    'wallet': dict(
+        num_demo_participants=1,
+        academy_game_name="Wallet App",
+        academy_wallet_create=True,
+    ),
+    'ultimatum': dict(
+        num_demo_participants=2,
+        academy_game_name="Ultimatum Game",
+    ),
+    'cournot': dict(
+        num_demo_participants=2,
+        academy_game_name="Cournot Game",
+    ),
+    'guess': dict(
+        num_demo_participants=2,
+        academy_game_name="Guessing Game",
+        academy_endcard_reward=200,
+    ),
+    'prisoner': dict(
+        num_demo_participants=2,
+        academy_game_name="Prisoner's Dilemma",
+    ),
+    'publicgood': dict(
+        num_demo_participants=4,
+        academy_game_name="Public Good Game",
+    ),
+    'auction': dict(
+        num_demo_participants=3,
+        academy_game_name="NFT Auction",
+    ),
+}
+
+
+# Helper class
+class AcademyGame:
+    """Caches metadata to generate otree configs."""
+
+    def __init__(self, identifier: str, config: dict):
+        """Load and cache game app module by identifier."""
+        self.id = identifier
+        self.name = f"academy_{identifier}"
+        self.config = config
+
+    @staticmethod
+    def make(identifier: str, config: dict) -> "AcademyGame":
+        """Construct game via identifier."""
+        return AcademyGame(identifier, config)
+
+    @property
+    def display_name(self) -> str:
+        """Fetch display name from config."""
+        return f"Academy Games - {self.game_name}"
+
+    @property
+    def game_name(self) -> str:
+        """Fetch game name from config."""
+        return self.config['academy_game_name']
+
+    @property
+    def app_sequence(self) -> list[str]:
+        """Determine app sequence of game."""
+        if self.id == "wallet":
+            return [self.name, "academy_endcard"]
+        else:
+            return ['academy_wallet', self.name, 'academy_endcard']
+
+    def session(self) -> dict:
+        """Return game as otree session config."""
+        return dict(
+            name=self.name,
+            display_name=self.display_name,
+            app_sequence=self.app_sequence,
+            academy_game_id=self.id,
+            academy_wallet_open=True,
+            academy_wallet_create=False,
+            academy_endcard_reward=400,
+        ) | self.config
+
+    def room(self) -> dict:
+        """Return game specific otree room config."""
+        return dict(
+            name=self.name,
+            display_name=self.display_name,
+        )
+
+
+# Cache modules for each game
+ACADEMY_GAMES = [AcademyGame.make(*kv) for kv in ACADEMY_GAME_CONFIGS.items()]
+
+# Generate session config for each game
+SESSION_CONFIGS = list(map(AcademyGame.session, ACADEMY_GAMES))
+
+# Generate room config for each game
+ROOMS = list(map(AcademyGame.room, ACADEMY_GAMES))
--- settings.py
+++ settings.py
@@ -1,143 +1,25 @@
+"""otree main config."""
+
+# Project-specific subconfigs
+import academy
+import auction
+
 from os import environ
 
 
-# Shared defaults between all auction sessions
-AUCTION_CONFIG_DEFAULTS = dict(
-    app_sequence=['slot_auction'],
-    real_world_currency_per_point=2.00, participation_fee=10.00,
-    doc="Please ensure adjustment of participants per treatment in 'Configure session' section.",
-    num_demo_participants=3,
-    num_hard_participants=0, num_candle_participants=0, num_activity_participants=0,
-    num_global_slots=2, num_local_slots=1,
-    shuffle_groups=False,
-)
-
-# Shared defaults to improve UI when creating academy sessions
-ACADEMY_CONFIG_DEFAULTS = dict(
-    academy_wallet_open=True,
-    academy_wallet_create=True,
-)
-
-SESSION_CONFIGS = [
-
-    # Configs to run actual studies
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_custom',
-        display_name="Auction Experiment",
-    ),
-
-    # Configs used to run academy sessions
-    ACADEMY_CONFIG_DEFAULTS | dict(
-        name='academy_one',
-        display_name="Academy Day 1",
-        app_sequence=[
-            'academy_wallet',
-            'academy_cournot',
-            'academy_endcard',
-        ],
-        num_demo_participants=2,
-        academy_wallet_open=False
-    ),
-
-    ACADEMY_CONFIG_DEFAULTS | dict(
-        name='academy_two',
-        display_name="Academy Day 2",
-        app_sequence=[
-            'academy_wallet',
-            'academy_guess',
-            'academy_prisoner',
-            'academy_punishment',
-            'academy_endcard',
-        ],
-        num_demo_participants=4,
-    ),
-
-    ACADEMY_CONFIG_DEFAULTS | dict(
-        name='academy_final',
-        display_name="Academy Auction Final",
-        app_sequence=[
-            'academy_wallet',
-            'academy_auction',
-        ],
-        real_world_currency_per_point=0.01,
-        num_demo_participants=4,
-        academy_wallet_create=False
-    ),
-
-    # Demos of academy games
-    dict(
-        name='cournot_demo',
-        app_sequence=['academy_cournot'],
-        display_name="Demo - Cournot Game",
-        num_demo_participants=2,
-    ),
-    dict(
-        name='guess_demo',
-        app_sequence=['academy_guess'],
-        display_name="Demo - Guess 2/3 Average",
-        num_demo_participants=4,
-    ),
-    dict(
-        name='prisoner_demo',
-        app_sequence=['academy_prisoner'],
-        display_name="Demo - Prisoner's Dilemma",
-        num_demo_participants=2,
-    ),
-    dict(
-        name='punishment_demo',
-        app_sequence=['academy_punishment'],
-        display_name="Demo - Common Goods with Punishment",
-        num_demo_participants=4,
-    ),
-
-    # Demo of auction treatments
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_hard',
-        display_name="Demo - Hard Ending Auction",
-        num_hard_participants=3,
-    ),
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_candle',
-        display_name="Demo - Candle Auction",
-        num_candle_participants=3,
-    ),
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_activity',
-        display_name="Demo - Activity Rule Auction",
-        num_activity_participants=3,
-    ),
-
-    # Demo of complex auction logic
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_more',
-        display_name="Demo - Three Slot Auction",
-        num_hard_participants=3,
-        num_global_slots=3,
-        num_local_slots=1,
-    ),
-    AUCTION_CONFIG_DEFAULTS | dict(
-        name='auction_complex',
-        display_name="Demo - Overlapping Slot Auction",
-        num_hard_participants=3,
-        num_global_slots=9,
-        num_local_slots=3,
-    ),
-]
-
-# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
-# in SESSION_CONFIGS, except those that explicitly override it.
-# the session config can be accessed from methods in your apps as self.session.config,
-# e.g. self.session.config['participation_fee']
-
+# Combine all session configs
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=0.00, participation_fee=0.00, num_demo_participants=0,
+    real_world_currency_per_point=0.00, participation_fee=0.00,
+    num_demo_participants=0,
 )
 
+SESSION_CONFIGS = academy.SESSION_CONFIGS + auction.SESSION_CONFIGS
+
+# Fields used across apps
 PARTICIPANT_FIELDS = ['role', 'treatment', 'finished']
 SESSION_FIELDS = ['reward_round']
 
 # ISO-639 code
-# for example: de, fr, ja, ko, zh-hans
 LANGUAGE_CODE = 'en'
 
 # e.g. EUR, GBP, CNY, JPY
@@ -152,13 +34,11 @@ DEMO_PAGE_INTRO_HTML = """<h4>Polkadot Experiments Platform</h4>"""
 
 SECRET_KEY = '3685133242197'
 
+# Combine all room configs
 ROOMS = [
+    # Default room for testing
     dict(
         name='testing',
         display_name='Testing & Development',
     ),
-    dict(
-        name='academy',
-        display_name='Blockchain Academy 2022',
-    ),
-]
+] + academy.ROOMS + auction.ROOMS

8091f862d9d6c86b177d462cb4d76bb37cc13225
Florian Franzen <Florian.Franzen@gmail.com>
2023-01-12 18:20:47 +0100

    prisoner: add chat log analysis

--- academy_prisoner/__init__.py
+++ academy_prisoner/__init__.py
@@ -3,10 +3,13 @@
 from otree.constants import BaseConstants
 from otree.currency import Currency
 from otree.models import BaseSubsession, BaseGroup, BasePlayer
+from otree.models_concrete import ChatMessage
 from otree.database import BooleanField
 
 from otree.views import Page, WaitPage
 from otree.forms import widgets
+
+from wallet import Wallet
 
 import json
 
@@ -126,6 +129,7 @@ page_sequence = [Introduction, DecisionWait, Decision, ResultsWait, Results]
 def vars_for_admin_report(subsession):
     session = subsession.session
 
+    # Compute cooperation ratios across rounds
     ratio = []
     for round in range(1, C.NUM_ROUNDS + 1):
         subsession = Subsession.objects_get(session=session, round_number=round)
@@ -146,7 +150,72 @@ def vars_for_admin_report(subsession):
         except:
             ratio += [None]
 
+    # Extract chat messages and outcome
+    all_names = {}
+    all_chats = {}
+
+    for round in range(1, C.NUM_ROUNDS + 1):
+        subsession = Subsession.objects_get(session=session, round_number=round)
+
+        # For each group this round...
+        for group in subsession.get_groups():
+            [p1, p2] = group.get_players()
+
+            channel = "{}-{}-{}".format(
+                session.id, C.NAME_IN_URL, group.id
+            )
+
+            #print(channel)
+
+            coop1 = p1.field_maybe_none("cooperate")
+            coop2 = p2.field_maybe_none("cooperate")
+
+            name1 = Wallet.current(p1.participant).public
+            name2 = Wallet.current(p2.participant).public
+
+
+            for msg in ChatMessage.objects_filter().order_by('timestamp'):
+                print(msg.channel)
+
+            # ... collect all message of group in round ...
+            messages = []
+            for msg in ChatMessage.objects_filter(channel=channel).order_by('timestamp'):
+
+                print(msg)
+
+                name = "Unknown"
+                color = None
+
+                if msg.participant == p1.participant:
+                    name = name1
+                    if coop1 is not None:
+                        color = "success" if coop1 else "danger"
+
+                elif msg.participant == p2.participant:
+                    name = name2
+                    if coop2 is not None:
+                        color = "success" if coop2 else "danger"
+
+                messages += [
+                    (name, color, msg.body)
+                ]
+
+            # ... and collect them for each participant pair
+            pp = p1.participant
+
+            if pp not in all_chats:
+                all_chats[pp] = []
+                all_names[pp] = (name1, name2)
+
+            all_chats[pp] += [(round, messages)]
+
+    chat_combined = []
+
+    for pp, messages in all_chats.items():
+        name1, name2 = all_names[pp]
+        chat_combined += [(name1, name2, messages)]
 
     return dict(
         coop=json.dumps(ratio),
+        all_chats=chat_combined,
     )
